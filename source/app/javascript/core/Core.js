104ef82c99c637c3f516b1526f2350b82348ac3c
Stephen Sylvia <ssylvia@esri.com>
2013-10-25 16:36:13 -0400

    Edit feature service

--- source/app/javascript/core/Core.js
+++ source/app/javascript/core/Core.js
@@ -1,5 +1,5 @@
-define(["storymaps/utils/MovableGraphic","dojo/_base/array","esri/arcgis/utils","esri/arcgis/Portal","esri/map","esri/dijit/Geocoder","esri/layers/GraphicsLayer","esri/graphic","esri/symbols/PictureMarkerSymbol","lib/jquery/jquery-1.10.2.min"],
-	function(MoveableGraphic,array,arcgisUtils,arcgisPortal,Map,Geocoder,GraphicsLayer,Graphic,PictureMarkerSymbol){
+define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/array","esri/arcgis/utils","esri/arcgis/Portal","esri/map","esri/dijit/Geocoder","esri/layers/GraphicsLayer","esri/graphic","esri/symbols/PictureMarkerSymbol","lib/jquery/jquery-1.10.2.min"],
+	function(MoveableGraphic,FeatureLayer,array,arcgisUtils,arcgisPortal,Map,Geocoder,GraphicsLayer,Graphic,PictureMarkerSymbol){
 
 		/**
 		* Core
@@ -10,39 +10,44 @@ define(["storymaps/utils/MovableGraphic","dojo/_base/array","esri/arcgis/utils",
 		* Dependencies: Jquery 1.10.2
 		*/
 
-		var _portal,
+		var _portal = new arcgisPortal.Portal("http://www.arcgis.com"),
+		_storyLayer = new FeatureLayer(configOptions.featureService),
 		_location;
 
 		function init ()
 		{
-			_portal = new arcgisPortal.Portal("http://www.arcgis.com");
 			login();
 		}
 
 		function login()
 		{
-			// _portal.signIn().then(function(result){
-			// 	var load = false;
-			// 	array.forEach(esri.id.credentials,function(user){
-			// 		if($.inArray(user.userId,configOptions.authorizedEditors) >= 0){
-			// 			load = true;
-			// 		}
-			// 	});
+			_portal.signIn().then(function(result){
+				var load = false;
+				array.forEach(esri.id.credentials,function(user){
+					if($.inArray(user.userId,configOptions.authorizedEditors) >= 0){
+						load = true;
+					}
+				});
 
-			// 	if (load){
+				if (load){
+					esri.id.getCredential(_storyLayer.url);
 					addFormEvents();
-			// 	}
-			// 	else{
-			// 		alert("You do not have permission to edit the World of Story Maps App.");
-			// 		location.reload();
-			// 	}
-			// });
+				}
+				else{
+					alert("You do not have permission to edit the World of Story Maps App.");
+					location.reload();
+				}
+			});
 		}
 
 		function addFormEvents()
 		{
 			$("#item-search").click(function(){
 				queryItem(getItemId($("#form-item").val()));
+			});
+
+			$("#item-add").click(function(){
+				addApplicaton();
 			});
 
 			$("#item-error-close").click(function(){
@@ -119,21 +124,89 @@ define(["storymaps/utils/MovableGraphic","dojo/_base/array","esri/arcgis/utils",
 		function queryItem(item)
 		{
 			var deferred = arcgisUtils.getItem(item).then(function(result){
-				$("#item-error").hide();
 				var item = result.item;
 				if (item.type === "Web Mapping Application"){
 					var thumbnail = "http://www.arcgis.com/sharing/rest/content/items/" + item.id + "/info/" + item.thumbnail;
+					$("#item-error").hide();
 					$("#form-name").val(item.title);
 					$("#form-description").val(item.snippet);
 					$("#form-publisher").val(item.owner);
 					$("#form-url").val(item.url);
 					$("#form-thumbnail").val(thumbnail);
 					$("#thumbnail-preview").attr("src",thumbnail);
-					window.portal = _portal;
+				}
+				else{
+					$("#item-error").show();
 				}
 			},function(){
 				$("#item-error").show();
 			});
+		}
+
+		function addApplicaton()
+		{
+			if(errorCheck()){
+				var attr = {
+					Name: $("#form-name").val(),
+					Description: $("#form-description").val(),
+					Publisher: $("#form-publisher").val(),
+					Story_URL: $("#form-url").val(),
+					Image_URL: $("#form-thumbnail").val(),
+					Template: $("#form-template").val()
+				}
+
+				var app = new Graphic(_location,null,attr);
+
+				_storyLayer.applyEdits([app]);
+			}
+		}
+
+		function errorCheck()
+		{
+			var noErrors = true;
+
+			if ($("#form-name").val() != ""){
+				$("#name-error").hide();
+			}
+			else{
+				$("#name-error").show();
+				noErrors = false;
+			}
+
+			if ($("#form-description").val() != ""){
+				$("#description-error").hide();
+			}
+			else{
+				$("#description-error").show();
+				noErrors = false;
+			}
+
+			if ($("#form-publisher").val() != ""){
+				$("#publisher-error").hide();
+			}
+			else{
+				$("#publisher-error").show();
+				noErrors = false;
+			}
+
+			if ($("#form-url").val() != ""){
+				$("#url-error").hide();
+			}
+			else{
+				$("#url-error").show();
+				noErrors = false;
+			}
+
+			if ($("#form-thumbnail").val() != ""){
+				$("#thumbnail-error").hide();
+			}
+			else{
+				$("#thumbnail-error").show();
+				noErrors = false;
+			}
+
+
+			return noErrors;
 		}
 
 		return {
