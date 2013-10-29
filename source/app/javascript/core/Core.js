2e4fc221cc4be9daa2b0c382b4f31db581a895f7
Stephen Sylvia <ssylvia@esri.com>
2013-10-29 12:20:31 -0400

    Include search and edit capability

--- source/app/javascript/core/Core.js
+++ source/app/javascript/core/Core.js
@@ -1,5 +1,5 @@
-define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/array","esri/arcgis/utils","esri/arcgis/Portal","esri/map","esri/dijit/Geocoder","esri/layers/GraphicsLayer","esri/graphic","esri/symbols/PictureMarkerSymbol","lib/jquery/jquery-1.10.2.min"],
-	function(MoveableGraphic,FeatureLayer,array,arcgisUtils,arcgisPortal,Map,Geocoder,GraphicsLayer,Graphic,PictureMarkerSymbol){
+define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/array","esri/arcgis/utils","esri/arcgis/Portal","esri/map","esri/tasks/query","esri/tasks/QueryTask","esri/dijit/Geocoder","esri/layers/GraphicsLayer","esri/graphic","esri/symbols/PictureMarkerSymbol","lib/jquery/jquery-1.10.2.min"],
+	function(MoveableGraphic,FeatureLayer,array,arcgisUtils,arcgisPortal,Map,Query,QueryTask,Geocoder,GraphicsLayer,Graphic,PictureMarkerSymbol){
 
 		/**
 		* Core
@@ -12,7 +12,10 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 
 		var _portal = new arcgisPortal.Portal("http://www.arcgis.com"),
 		_storyLayer = new FeatureLayer(configOptions.featureService),
-		_location;
+		_location,
+		_tempLocation,
+		_tempGraphic,
+		map;
 
 		function init ()
 		{
@@ -46,8 +49,32 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 				queryItem(getItemId($("#form-item").val()));
 			});
 
+			$("#form-item").keypress(function(event){
+				if(event.which === 13){
+					queryItem(getItemId($("#form-item").val()));
+				}
+			});
+
+			$("#item-search-submit").click(function(){
+				searchItems($("#form-item-search").val());
+			});
+
+			$("#form-item-search").keypress(function(event){
+				if(event.which === 13){
+					searchItems($("#form-item-search").val());
+				}
+			});
+
 			$("#item-add").click(function(){
 				addApplicaton();
+			});
+
+			$("#item-edit").click(function(){
+				editApplicaton();
+			});
+
+			$("#item-delete").click(function(){
+				deleteApplicaton();
 			});
 
 			$("#item-error-close").click(function(){
@@ -58,12 +85,17 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 				$("#thumbnail-preview").attr("src",$("#form-thumbnail").val());
 			});
 
+			$("body").scroll(function(){
+				map.resize();
+				map.reposition();
+			});
+
 			createMap();
 		}
 
 		function createMap()
 		{
-			var map = new Map("map",{
+			map = new Map("map",{
 				basemap: "topo",
 				center: [-19,32],
 				zoom: 2
@@ -79,20 +111,20 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 			}
 		}
 
-		function addMapInterface(map)
+		function addMapInterface()
 		{
 			var graphicsLayer = new GraphicsLayer();
 			map.addLayer(graphicsLayer);
 
 			var pt = map.extent.getCenter();
-			var sym = new PictureMarkerSymbol("resources/images/RedPin.png",30,35).setOffset(14,7);
+			var sym = new PictureMarkerSymbol("resources/images/RedPin.png",30,35).setOffset(0,7);
 
-			var location = new Graphic(pt,sym);
-			graphicsLayer.add(location);
+			_tempLocation = new Graphic(pt,sym);
+			graphicsLayer.add(_tempLocation);
 
 			_location = pt;
 
-			var movable = new MoveableGraphic(map,graphicsLayer,location,null,function(graphic){
+			var movable = new MoveableGraphic(map,graphicsLayer,_tempLocation,function(graphic){
 				_location = graphic.geometry;
 			});
 
@@ -102,9 +134,8 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 			},"geocoder");
 
 			geocoder.on("select",function(result){
-				console.log(result);
 				_location = result.result.feature.geometry;
-				location.setGeometry(_location);
+				_tempLocation.setGeometry(_location);
 			});
 		}
 
@@ -121,15 +152,67 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 			}
 		}
 
+		function searchItems(str)
+		{
+			$(".search-message").show();
+			var query = new Query();
+			query.outFields = ["*"];
+			query.returnGeometry = true;
+			query.where = "Name LIKE '%" + str + "%' OR Template LIKE '%" + str + "%' OR Description LIKE '%" + str + "%' OR Publisher LIKE '%" + str + "%'";
+
+			var queryTask = new QueryTask(_storyLayer.url);
+			queryTask.execute(query,function(result){			
+
+				$(".results tbody").empty();
+
+				array.forEach(result.features,function(ftr){
+					$(".results tbody").append('\
+						<tr>\
+							<td><span class="icon-edit btn">Edit</span></td>\
+							<td>' + ftr.attributes.Name + '</td>\
+							<td>' + ftr.attributes.Description + '</td>\
+							<td>' + ftr.attributes.Publisher + '</td>\
+						</tr>\
+					');
+
+					$(".results tbody .icon-edit").last().data("graphic",ftr).click(function(){
+						var item = $(this).data("graphic").attributes;
+						_tempGraphic = $(this).data("graphic");
+						$("#form-name").val(item.Name);
+						$("#form-description").val(item.Description);
+						$("#form-publisher").val(item.Publisher);
+						$("#form-url").val(item.Story_URL);
+						$("#form-thumbnail").val(item.Image_URL);
+						$("#form-template").val(item.Template);
+						$("#thumbnail-preview").attr("src",item.Image_URL);
+						_tempLocation.setGeometry(_tempGraphic.geometry);
+
+						map.resize();
+						map.reposition();
+					});
+
+				});
+
+				$(".results").show();
+				$(".search-message").hide();
+
+				map.resize();
+				map.reposition();
+			});
+		}
+
 		function queryItem(item)
 		{
+			$(".search-message").show();
 			var deferred = arcgisUtils.getItem(item).then(function(result){
+				$(".search-message").hide();
 				var item = result.item;
+				console.log(item);
 				if (item.type === "Web Mapping Application"){
 					var thumbnail = "http://www.arcgis.com/sharing/rest/content/items/" + item.id + "/info/" + item.thumbnail;
 					$("#item-error").hide();
 					$("#form-name").val(item.title);
-					$("#form-description").val(item.snippet);
+					$("#form-description").val(item.description);
 					$("#form-publisher").val(item.owner);
 					$("#form-url").val(item.url);
 					$("#form-thumbnail").val(thumbnail);
@@ -156,8 +239,104 @@ define(["storymaps/utils/MovableGraphic","esri/layers/FeatureLayer","dojo/_base/
 				}
 
 				var app = new Graphic(_location,null,attr);
+				console.log(app);
 
-				_storyLayer.applyEdits([app]);
+				$(".upload-message").hide();
+				$(".upload-message.sync").show();
+
+				_storyLayer.applyEdits([app]).then(function(result){
+					var error = false;
+					array.forEach(result,function(r){
+						if (!r.success){
+							error = true;
+							console.log(r.error);
+						}
+					});
+					if(error){
+						$(".upload-message").hide();
+						$(".upload-message.error").show();
+					}
+					else{
+						$(".upload-message").hide();
+						$(".upload-message.success").show();
+					}
+				},function(error){
+					$(".upload-message").hide();
+					$(".upload-message.error").show();
+				});
+			}
+		}
+
+		function editApplicaton()
+		{
+			if(errorCheck()){
+				var attr = {
+					FID: _tempGraphic.attributes.FID,
+					Name: $("#form-name").val(),
+					Description: $("#form-description").val(),
+					Publisher: $("#form-publisher").val(),
+					Story_URL: $("#form-url").val(),
+					Image_URL: $("#form-thumbnail").val(),
+					Template: $("#form-template").val()
+				}
+
+				var app = new Graphic(_location,null,attr);
+
+				$(".edit-message").hide();
+				$(".delete-message").hide();
+				$(".edit-message.sync").show();
+
+				_storyLayer.applyEdits(null,[app]).then(function(result){
+					var error = false;
+					array.forEach(result,function(r){
+						if (!r.success){
+							error = true;
+							console.log(r.error);
+						}
+					});
+					if(error){
+						$(".edit-message").hide();
+						$(".edit-message.error").show();
+					}
+					else{
+						$(".edit-message").hide();
+						$(".edit-message.success").show();
+					}
+				},function(error){
+					$(".edit-message").hide();
+					$(".edit-message.error").show();
+				});
+			}
+		}
+
+		function deleteApplicaton()
+		{
+			if(confirm("Are you sure you want to delete this application from the World of Story Maps?")){
+
+				$(".edit-message").hide();
+				$(".delete-message").hide();
+				$(".delete-message.sync").show();
+
+				_storyLayer.applyEdits(null,null,[_tempGraphic]).then(function(result){
+					var error = false;
+					array.forEach(result,function(r){
+						if (!r.success){
+							error = true;
+							console.log(r.error);
+						}
+					});
+					if(error){
+						$(".delete-message").hide();
+						$(".delete-message.error").show();
+					}
+					else{
+						$(".delete-message").hide();
+						$(".delete-message.success").show();
+					}
+				},function(error){
+					$(".delete-message").hide();
+					$(".delete-message.error").show();
+				});
 			}
 		}
 
